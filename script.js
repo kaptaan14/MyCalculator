b9d4027b527af94528c07e7d3837f73a8758b503
kaptaan14 <mrashishthakur2@gmail.com>
2023-05-22 20:36:45 +0530

    make personal calculator to practise html,css and js

---
+++ script.js
@@ -0,0 +1,32 @@
+let string = "";
+
+let buttony = document.querySelectorAll('.buttons');
+Array.from(buttony).forEach((buttons) => {
+    buttons.addEventListener('click', (e) => {
+        try{
+        let values = e.target.value;
+        if (values == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (values == 'c') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if (values == 'd') {
+            var newStr = string.substring(0, string.length - 1);
+            string = newStr;
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(values)
+            string = string + values;
+            document.querySelector('input').value = string;
+        }
+        string = document.querySelector('input').value;
+    }
+    catch{
+        document.querySelector('input').value = "";
+    }
+    })
+})
